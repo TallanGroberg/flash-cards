78289335e9da03a7a8e26c8f95f89cd1f0ffe994
tallan groberg <tallan.taven@gmail.com>
2019-05-31 17:16:49 -0600

    can remove cards

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("test");
+$("document").ready(function() {
+
+});
+$(".card1").on("click", function() {
+  $("#c1").hide();
+});
+$(".card2").on("click", function() {
+  $("#c2").hide();
+});
+
+
+var make = document.getElementById("make-card");
+
+make.getElementById("")
